d7af5092376b64545dc69caf1fd750bd40824e4f
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2023-04-10 17:03:13 +0200

    corrected a bug in preprocessing

--- src/biom3d/preprocess.py
+++ src/biom3d/preprocess.py
@@ -70,28 +70,38 @@ def one_hot(values, num_classes=None):
 def one_hot_fast(values, num_classes=None):
     """
     transform the 'values' array into a one_hot encoded one
-    """
-    if num_classes==None: n_values = np.max(values) + 1
-    else: n_values = num_classes
 
+    Warning ! If the number of unique values in the input array is lower than the number of classes, then it will consider that the array values are all between zero and `num_classes`. If one value is greater than `num_classes`, then it will add missing values systematically after the maximum value, which could not be the expected behavior. 
+    """
     # get unique values
-    uni = np.sort(np.unique(values))
+    uni = np.sort(np.unique(values)).astype(np.uint8)
+
+    if num_classes==None: 
+        n_values = len(uni)
+    else: 
+        n_values = num_classes
     
-    # if the expected number of class is two then apply a threshold
-    if len(uni)>2 and n_values==2:
-        values = (values>uni[0]).astype(np.uint8)
-    
-    # add values if uni is incomplete
-    while len(uni)<n_values: 
-        uni = np.append(uni, np.uint8(uni[-1]+1))
+        # if the expected number of class is two then apply a threshold
+        if n_values==2 and (len(uni)>2 or uni.max()>1):
+            print("[Warning] The number of expected values is 2 but the maximum value is higher than 1. Threshold will be applied.")
+            values = (values>uni[0]).astype(np.uint8)
+            uni = np.array([0,1]).astype(np.uint8)
+        
+        # add values if uni is incomplete
+        if len(uni)<n_values: 
+            # if the maximum value of the array is greater than n_value, it might be an error but still, we add values in the end.
+            if values.max() >= n_values:
+                print("[Warning] The maximum values in the array is greater than the provided number of classes, this might be unexpected and might cause issues.")
+                while len(uni)<n_values:
+                    uni = np.append(uni, np.uint8(uni[-1]+1))
+            # add missing values in the array by considering that each values are in 0 and n_value
+            else:
+                uni = np.arange(0,n_values).astype(np.uint8)
         
     # create the one-hot encoded matrix
-    out = np.empty((n_values, *values.shape), dtype=np.uint8)
-    c = 0
+    out = np.zeros((n_values, *values.shape), dtype=np.uint8)
     for i in range(n_values):
-        if i in uni:
-            out[i] = (values==uni[c]).astype(np.uint8)
-            c += 1
+        out[i] = (values==uni[i]).astype(np.uint8)
     return out
 
 class Preprocessing:
@@ -375,6 +385,7 @@ if __name__=='__main__':
         num_classes=args.num_classes+1,
         remove_bg=not args.add_bg,
         use_tif=not args.use_npy,
+        # clipping_bounds=[-928.0,296.0]
     )
 
     p.prepare()
