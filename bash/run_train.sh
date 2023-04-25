e6003a23c24a4775919321c345f1cb68148f676e
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2023-04-25 16:44:17 +0200

    updated one hot encoding in utils

--- bash/run_train.sh
+++ bash/run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH -o ./slurm/%j-train.out # STDOUT
 
-# python -m biom3d.train --config configs/20230413-unet_btcv.py
-python -m biom3d.train --log logs/20230422-123416-unet_btcv
+python -m biom3d.train --config configs/20230413-unet_btcv.py
+# python -m biom3d.train --log logs/20230422-123416-unet_btcv
 # python -m biom3d.train --config configs/20230421-141639-config_default.py
