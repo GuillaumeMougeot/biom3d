6e11ec3c8e3632e5c30303ee0866018c5dbde6a3
Sami SAFARBATI <samsafarbati@hotmail.com>
2024-07-11 13:22:42 +0200

    num_worker to 1

--- src/biom3d/config_default.py
+++ src/biom3d/config_default.py
@@ -115,7 +115,7 @@ USE_SOFTMAX = True
 
 # training loop parameters
 USE_FP16 = True
-NUM_WORKERS = 6
+NUM_WORKERS = 1
 PIN_MEMORY = True
 
 #---------------------------------------------------------------------------
