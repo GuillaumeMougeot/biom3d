32700355788094f8babdef9054bf7a0a48d887f7
Clement COLMERAUER <clcolmer>
2025-04-03 14:59:46 +0200

    Deleted packager.py, Argument managment

--- src/biom3d/package.py
+++ src/biom3d/package.py
@@ -1,5 +1,16 @@
-def package():
+import argparse
+
+def contains_model(path_to_model):
+    pass
+
+def package(path_to_model):
     pass
 
 if __name__=='__main__':
-    package()
+    parser = argparse.ArgumentParser(description="Package model.")
+    parser.add_argument("model_dir",help="Path to model directory")  
+    args = parser.parse_args()
+    try:
+        package(args.model_dir)
+    except FileNotFoundError :
+        print("The given directory doesn't contain a model")
